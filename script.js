e2b75d92000e16a073f683dd2170f7c12cdc139c
franbuesaaa <89xzw7z7zw@privaterelay.appleid.com>
2025-12-19 02:17:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -161,21 +161,12 @@ function render() {
 
       r.textContent = ramo.nombre;
 
-      // TOCAR: aprobar
       r.onclick = () => {
-        if (r.classList.contains("activo") && !aprobados.includes(ramo.nombre)) {
+        if (r.classList.contains("activo")) {
           aprobados.push(ramo.nombre);
           localStorage.setItem("aprobados", JSON.stringify(aprobados));
           render();
         }
-      };
-
-      // MANTENER APRETADO: desmarcar SOLO este ramo
-      r.oncontextmenu = (e) => {
-        e.preventDefault();
-        aprobados = aprobados.filter(a => a !== ramo.nombre);
-        localStorage.setItem("aprobados", JSON.stringify(aprobados));
-        render();
       };
 
       div.appendChild(r);
